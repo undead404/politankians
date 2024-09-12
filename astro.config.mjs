381c02528cf1cd9ceaf0b6df0679ef45db77aa37
Vitalii Perehonchuk <vitalii_perehonchuk@epam.com>
2024-09-12 10:53:27 +0300

    feat: sitemap & favicon

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,10 +2,12 @@ import sitemap from '@astrojs/sitemap';
 import { defineConfig } from 'astro/config';
 import * as dotenv from 'dotenv';
 
+import icon from 'astro-icon';
+
 dotenv.config();
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [sitemap()],
+  integrations: [sitemap(), icon()],
   site: process.env.SITE,
 });
