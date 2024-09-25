436c38141c22524c841e3c35e67de5e9c23b49b0
Vitalii Perehonchuk <vitalii_perehonchuk@epam.com>
2024-09-25 16:47:13 +0300

    feat: import data into Typesense in chunks

--- src/populate-typesense/populate-index.ts
+++ src/populate-typesense/populate-index.ts
@@ -1,7 +1,13 @@
+import _ from 'lodash';
 import type { Act } from '../schemas/act.js';
 
 import typesense from './typesense.js';
 
+const CHUNK_SIZE = 5000;
+
 export default async function populateIndex(data: Act[]) {
-  return typesense.collections('acts').documents().import(data);
+  const chunks = _.chunk(data, CHUNK_SIZE);
+  for (const chunk of chunks) {
+    await typesense.collections('acts').documents().import(chunk);
+  }
 }
